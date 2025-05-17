dd1a7776073baeed4fa5715001e4ea759cb1fd62
Meoshameka Taylor <m_taylor77@me.com>
2025-05-17 12:38:40 -0500

    email error

--- app.js
+++ app.js
@@ -10,14 +10,43 @@ function closeMenu(){
    body.classList.remove("open")
 }
 
-//template_eic28s7
+//template_eb0nrkt
 //service_f4jfbvf
 //zyNoGk1qjZ0y3lWnH
 
-function sendEmail(event){
-    event.preventDefault()
+async function sendEmail(event){
+    event.preventDefault();
+    const body = document.body;
+    const loading = document.querySelector('.contact__form__loading');
+const form = document.querySelector('.contact__form')
 
-    emailjs.send("service_f4jfbvf", "template_eic28s7", event.target, "zyNoGk1qjZ0y3lWnH");
+try{
+    // Loading state
+loading.classList.remove('hiddnen');
 
-    
-    
+await emailjs.sendForm(
+       "service_f4jfbvf", 
+       "template_eb0nrkt", 
+       event.target, 
+       "zyNoGk1qjZ0y3lWnH"
+   );
+
+//Success state
+form.reset();
+loading.classList.add("hidden")
+body.classList.add("success-open")
+setTimeout(() => {
+   body.classList.remove("success-open")
+}, 5000)
+console.log('hey the email has been sent!')
+
+} catch{
+    loading.classList.add("hidden");
+    alert(
+        "An error has occured. Please try again later or contact me at meoshameka@gmail.com."
+    );
+   
+}
+
+}
+
