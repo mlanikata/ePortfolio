a34cd9ca98f51015345f451d5b4436f161684f8c
Meoshameka Taylor <m_taylor77@me.com>
2025-05-17 12:53:42 -0500

    email error2

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
-function openMenu(){
-const body = document.body
+function openMenu() {
+  const body = document.body;
 
-body.classList.add("open")
+  body.classList.add('open');
 }
 
-function closeMenu(){
-    const body = document.body
+function closeMenu() {
+  const body = document.body;
 
-   body.classList.remove("open")
+  body.classList.remove('open');
 }
 
 //template_eb0nrkt
 //service_f4jfbvf
 //zyNoGk1qjZ0y3lWnH
 
-async function sendEmail(event){
-    event.preventDefault();
-    const body = document.body;
-    const loading = document.querySelector('.contact__form__loading');
-const form = document.querySelector('.contact__form')
+async function sendEmail(event) {
+  event.preventDefault();
+  const body = document.body;
+  const loading = document.querySelector('.contact__form__loading');
+  const form = document.querySelector('.contact__form');
 
-try{
+  try {
     // Loading state
-loading.classList.remove('hiddnen');
+    loading.classList.remove('hidden');
 
-await emailjs.sendForm(
-       "service_f4jfbvf", 
-       "template_eb0nrkt", 
-       event.target, 
-       "zyNoGk1qjZ0y3lWnH"
-   );
-
-//Success state
-form.reset();
-loading.classList.add("hidden")
-body.classList.add("success-open")
-setTimeout(() => {
-   body.classList.remove("success-open")
-}, 5000)
-console.log('hey the email has been sent!')
-
-} catch{
-    loading.classList.add("hidden");
-    alert(
-        "An error has occured. Please try again later or contact me at meoshameka@gmail.com."
+    await emailjs.sendForm(
+      'service_f4jfbvf',
+      'template_eb0nrkt',
+      event.target,
+      'zyNoGk1qjZ0y3lWnH'
     );
-   
-}
 
+    //Success state
+    form.reset();
+    loading.classList.add('hidden');
+    body.classList.add('success-open');
+    setTimeout(() => {
+      body.classList.remove('success-open');
+    }, 5000);
+  } catch {
+    loading.classList.add('hidden');
+    alert(
+      'An error has occured. Please try again later or contact me at meoshameka@gmail.com.'
+    );
+  }
 }
-
